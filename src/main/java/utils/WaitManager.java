5f278b9a004a95113375a0a149fd33ce9b8211a6
OmarGamal <109871074+OmarGamall@users.noreply.github.com>
2026-06-21 02:46:43 +0300

    implement Waits Manager using custom fluent wait

--- src/main/java/utils/WaitManager.java
+++ src/main/java/utils/WaitManager.java
@@ -1,4 +1,33 @@
 package utils;
 
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.FluentWait;
+
+import java.time.Duration;
+import java.util.ArrayList;
+
 public class WaitManager {
+    private static WebDriver driver;
+
+    public WaitManager(WebDriver driver) {
+        WaitManager.driver = driver;
+    }
+
+    public static FluentWait<WebDriver> fluentWait() {
+        return new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(10)) //wait for 10 seconds
+                .pollingEvery(Duration.ofMillis(500))
+                .ignoreAll(getExceptions());
+
+    }
+
+    // Define the exceptions to be ignored
+    private static ArrayList<Class<? extends Exception>> getExceptions() {
+        ArrayList<Class<? extends Exception>> exceptions = new ArrayList<>();
+        exceptions.add(NoSuchElementException.class);
+        exceptions.add(StaleElementReferenceException.class);
+        exceptions.add(ElementNotInteractableException.class);
+        exceptions.add(ElementClickInterceptedException.class);
+        return exceptions;
+    }
 }
